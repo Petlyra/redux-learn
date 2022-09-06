c55cca94333b1cadcaa2790c83e01e22dee15a8a
Petlyra <bazkus34>
2022-09-06 17:55:37 +0300

    rename store

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { configureStore } from "./redux/root";
+import { configureStore } from "./redux/store";
 import { persistStore } from "redux-persist";
 
 const root = ReactDOM.createRoot(
