f6340282e86e39f514be8fbe9f4037bc32c39745
Petlyra <bazkus34>
2022-09-06 17:27:17 +0300

    todo item fix

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,10 +3,7 @@ import { TodoType } from "../types/todo-type";
 
 export const TodoItem = (todo: TodoType) => {
   return (
-    <div
-      key={todo.id}
-      style={{ display: "flex", flexDirection: "column", fontSize: 16 }}
-    >
+    <div key={todo.id} style={{ fontSize: 16 }}>
       {JSON.stringify(todo)}
     </div>
   );
